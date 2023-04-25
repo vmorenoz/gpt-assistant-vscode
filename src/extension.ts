f9c77d5ef3ab9f0218298eea73ea311a92929cf9
Victor Moreno Zapata <vmorenoz@proveedor.intercorp.com.pe>
2023-04-24 21:03:05 -0500

    feat: change model

--- src/extension.ts
+++ src/extension.ts
@@ -1,114 +1,165 @@
-import * as vscode from 'vscode';
+import { log } from "console";
+import * as vscode from "vscode";
 const { Configuration, OpenAIApi } = require("openai");
 
-const config = vscode.workspace.getConfiguration('gptAssistant');
-const apiKey = config.get<string>('apiKey') || '';
-const maxTokens = config.get<number>('maxTokens') || 2000;
-const unitTestsPrompt = config.get<string>('unitTestsPrompt') || '';
-const refactoringPrompt = config.get<string>('refactoringPrompt') || '';
-const model = config.get<string>('model') || '';
+const config = vscode.workspace.getConfiguration("gptAssistant");
+const apiKey = config.get<string>("apiKey") || "";
+const lang = config.get<string>("lang") || "";
+const maxTokens = config.get<number>("maxTokens") || 2000;
+const unitTestsCharacterPrompt = (
+  config.get<string>("unitTestsCharacterPrompt") || ""
+).replace("{LANG}", lang);
+const refactoringCharacterPrompt = (
+  config.get<string>("refactoringCharacterPrompt") || ""
+).replace("{LANG}", lang);
+const model = config.get<string>("model") || "";
+const actionPrompts = {
+  unitTests:
+    "Analyze the following code and generate efficient unit tests with high code coverage, give back only {LANG} code without natural language descriptions, Here is the code:".replace('{LANG}', lang),
+  refactoring:
+    "Refactor the following code to make it cleaner, more maintainable, SOLID principies, DRY and efficient, without losing any functionality, give back only {LANG} code without natural language descriptions, Here is the code:".replace('{LANG}', lang),
+};
 
 const configuration = new Configuration({
-	apiKey
+  apiKey,
 });
 const openai = new OpenAIApi(configuration);
 
 export function activate(context: vscode.ExtensionContext) {
-	let unitTestsCommand = vscode.commands.registerCommand('gpt-assistant.generateUnitTests', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No hay un editor activo.');
-			return;
-		}
+  let unitTestsCommand = vscode.commands.registerCommand(
+    "gpt-assistant.generateUnitTests",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No hay un editor activo.");
+        return;
+      }
 
-		const selectedText = editor.document.getText(editor.selection);
+      const selectedText = editor.document.getText(editor.selection);
 
-		if (selectedText.length === 0) {
-			vscode.window.showErrorMessage('Por favor, selecciona un fragmento de código antes de generar pruebas unitarias.');
-			return;
-		}
+      if (selectedText.length === 0) {
+        vscode.window.showErrorMessage(
+          "Por favor, selecciona un fragmento de código antes de generar pruebas unitarias."
+        );
+        return;
+      }
 
-		callGpt(selectedText, unitTestsPrompt, 'Generando pruebas unitarias...', 'No se pudo generar ninguna prueba unitaria', 'Error al generar pruebas unitarias',
-			(code: string) => {
-				displayResponse(code, 'Pruebas unitarias generadas con éxito');
-			});
-	});
+      callGpt(
+        selectedText,
+        unitTestsCharacterPrompt,
+        actionPrompts.unitTests,
+        "Generando pruebas unitarias...",
+        "No se pudo generar ninguna prueba unitaria",
+        "Error al generar pruebas unitarias",
+        (code: string) => {
+          displayResponse(code, "Pruebas unitarias generadas con éxito");
+        }
+      );
+    }
+  );
 
-	let refactoringCommand = vscode.commands.registerCommand('gpt-assistant.refactoring', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No hay un editor activo.');
-			return;
-		}
+  let refactoringCommand = vscode.commands.registerCommand(
+    "gpt-assistant.refactoring",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No hay un editor activo.");
+        return;
+      }
 
-		const selectedText = editor.document.getText(editor.selection);
+      const selectedText = editor.document.getText(editor.selection);
 
-		if (selectedText.length === 0) {
-			vscode.window.showErrorMessage('Por favor, selecciona un fragmento de código antes de refactorizar.');
-			return;
-		}
+      if (selectedText.length === 0) {
+        vscode.window.showErrorMessage(
+          "Por favor, selecciona un fragmento de código antes de refactorizar."
+        );
+        return;
+      }
 
-		callGpt(selectedText, refactoringPrompt, 'Refactorizando...', 'No se pudo refactorizar', 'Error al refactorizar',
-			(code: string) => {
-				displayResponse(code, 'Código refactorizado con éxito');
-			});
-	});
+      callGpt(
+        selectedText,
+        refactoringCharacterPrompt,
+        actionPrompts.refactoring,
+        "Refactorizando...",
+        "No se pudo refactorizar",
+        "Error al refactorizar",
+        (code: string) => {
+          displayResponse(code, "Código refactorizado con éxito");
+        }
+      );
+    }
+  );
 
-	context.subscriptions.push(unitTestsCommand, refactoringCommand);
+  context.subscriptions.push(unitTestsCommand, refactoringCommand);
 }
 
 async function callGpt(
-	selectedCode: string,
-	actionPrompt: string,
-	title: string,
-	generationError: string,
-	operationError: string,
-	displayCallback: (code: string) => void) {
-	const prompt = `${actionPrompt} ${selectedCode}`;
+  code: string,
+  characterPromt: string,
+  actionPrompt: string,
+  title: string,
+  generationError: string,
+  operationError: string,
+  displayCallback: (code: string) => void
+) {
+  await vscode.window.withProgress(
+    {
+      location: vscode.ProgressLocation.Window,
+      title,
+      cancellable: false,
+    },
+    async (progress) => {
+      try {
+        console.log(`Starting GPT with model: ${model}`);
 
-	await vscode.window.withProgress(
-		{
-			location: vscode.ProgressLocation.Window,
-			title,
-			cancellable: false,
-		},
-		async (progress) => {
-			try {
-				const response = await openai.createCompletion({
-					model,
-					prompt,
-					temperature: 0,
-					max_tokens: maxTokens,
-				});
+        var messages = [
+          {
+            role: "system",
+            content: "Stay in character. " + characterPromt + 'and response without natural language dialogs, only code generated.',
+          },
+          {
+            role: "user",
+            content: `${actionPrompt} \n${code}`,
+          },
+        ];
 
-				const choices = response.data.choices;
-				if (choices && choices.length > 0) {
-					const responseCode = choices[0].text.trim();
-					displayCallback(responseCode);
-				} else {
-					vscode.window.showErrorMessage(generationError);
-				}
-			} catch (error: any) {
-				vscode.window.showErrorMessage(`${operationError}: ${error.message}`);
-			}
-		}
-	);
+        const completion = await openai.createChatCompletion({
+          model,
+          messages,
+          temperature: 0.5,
+          max_tokens: maxTokens,
+        });
 
+        var choices = completion.data.choices;
 
+        if (choices && choices.length > 0) {
+          const responseCode = choices[0].message.content;
+          displayCallback(responseCode);
+        } else {
+          vscode.window.showErrorMessage(generationError);
+        }
+      } catch (error: any) {
+        console.log(`Error: ${error}`);
+        vscode.window.showErrorMessage(`${operationError}: ${error}`);
+      }
+    }
+  );
 }
 
 async function displayResponse(code: string, sucessMessage: string) {
-	// Abrir un nuevo editor sin guardar el archivo y pegar el código allí
-	const untitledFileUri = vscode.Uri.parse('untitled:result.ts');
-	const newDocument = await vscode.workspace.openTextDocument(untitledFileUri);
-	const editor = await vscode.window.showTextDocument(newDocument, vscode.ViewColumn.Beside);
+  // Abrir un nuevo editor sin guardar el archivo y pegar el código allí
+  const untitledFileUri = vscode.Uri.parse("untitled:result.ts");
+  const newDocument = await vscode.workspace.openTextDocument(untitledFileUri);
+  const editor = await vscode.window.showTextDocument(
+    newDocument,
+    vscode.ViewColumn.Beside
+  );
 
-	// Insertar el código de las pruebas unitarias en el nuevo editor
-	await editor.edit((editBuilder) => {
-		editBuilder.insert(newDocument.lineAt(0).range.start, code);
-		vscode.window.showInformationMessage(sucessMessage);
-	});
+  // Insertar el código de las pruebas unitarias en el nuevo editor
+  await editor.edit((editBuilder) => {
+    editBuilder.insert(newDocument.lineAt(0).range.start, code);
+    vscode.window.showInformationMessage(sucessMessage);
+  });
 }
 
-
-export function deactivate() { }
+export function deactivate() {}
